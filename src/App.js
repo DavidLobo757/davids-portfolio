7177d406e509c5fc859b41ae7ae103da1ae577d6
rodzdev1971 <102566113+rodzdev1971@users.noreply.github.com>
2023-05-14 13:55:27 -0400

    Update App.js
    Just testing

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route  path="/" element={<TodoApp/>}/>
-      <Route exact path="/dashboard" element={<Dashboard/>}/>
+      <Route exact path="/dashboard" element={<Dashboard/>}/>//Change Routing methods
     </Routes>
   );
 }
